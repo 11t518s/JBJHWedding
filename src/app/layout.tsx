e380784e4d491c856b89ee41c14de6586497967a
bongsu-rapportlabs <bongsu@rapportlabs.kr>
2024-03-06 01:23:14 +0900

    test5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
   title: "💌 정빈 & 정화 결혼식에 초대합니다!",
   description: `5월 18일 토요일 오후 5시\n아펠가모 공덕 7층 라로브홀`,
   openGraph: {
-    images: ["/OG.png"],
+    images: process.env.NEXT_PUBLIC_PUBLIC_URL + "/OG.png",
   },
 };
 
