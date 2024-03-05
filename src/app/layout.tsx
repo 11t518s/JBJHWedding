e7ab2b289e8335919dc16fb201f5b2589bba975b
bongsu-rapportlabs <bongsu@rapportlabs.kr>
2024-03-06 01:27:09 +0900

    description test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,15 @@ import type { Metadata } from "next";
 import styles from "@/styles/global.module.css";
 import Script from "next/script";
 
+const title = "💌 정빈 & 정화 결혼식에 초대합니다!";
+const description = `5월 18일 토요일 오후 5시\n아펠가모 공덕 7층 라로브홀`;
+
 export const metadata: Metadata = {
-  title: "💌 정빈 & 정화 결혼식에 초대합니다!",
-  description: `5월 18일 토요일 오후 5시\n아펠가모 공덕 7층 라로브홀`,
+  title,
+  description,
   openGraph: {
+    title,
+    description,
     images: process.env.NEXT_PUBLIC_PUBLIC_URL + "/OG.png",
   },
 };
