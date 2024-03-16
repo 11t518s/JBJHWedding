3e416d8f0a1cbd2189918887be95297bc7f9d2b1
bongsu-rapportlabs <bongsu@rapportlabs.kr>
2024-03-16 21:22:23 +0900

    wip

--- src/design-system/Accordion/index.tsx
+++ src/design-system/Accordion/index.tsx
@@ -23,67 +23,69 @@ export const Accordion = ({
 }: AccordionProps) => {
   const [isOpen, setIsOpen] = useState(initiallyOpen);
   return (
-    <motion.div
-      style={{
-        border: "2px solid",
-        borderRadius: "12px",
-        padding: "20px",
-        width: "calc(100vw - 40px)",
-        maxWidth: "680px",
-        boxSizing: "border-box",
-      }}
-    >
-      <motion.header
+    <AnimatePresence initial={false}>
+      <motion.div
         style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
+          border: "2px solid",
+          borderRadius: "12px",
+          padding: "20px",
+          width: "calc(100vw - 40px)",
+          maxWidth: "680px",
+          boxSizing: "border-box",
         }}
-        initial={false}
-        onClick={() => setIsOpen((prev) => !prev)}
       >
-        {header}
-        {isOpen ? (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.5 }}
-          >
-            <Icons iconName="chevronUp" />
-          </motion.div>
-        ) : null}
-        {!isOpen ? (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.5 }}
-          >
-            <Icons iconName="chevronDown" />
-          </motion.div>
-        ) : null}
-      </motion.header>
-      <AnimatePresence initial={false}>
-        {isOpen ? (
-          <motion.section
-            key="content"
-            initial="collapsed"
-            animate="open"
-            exit="collapsed"
-            variants={{
-              open: { opacity: 1, height: "auto" },
-              collapsed: { opacity: 0, height: 0 },
-            }}
-            transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
-            style={{
-              overflow: "hidden",
-            }}
-          >
-            {content}
-          </motion.section>
-        ) : null}
-      </AnimatePresence>
-    </motion.div>
+        <motion.header
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+          initial={false}
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          {header}
+          {isOpen ? (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.5 }}
+            >
+              <Icons iconName="chevronUp" />
+            </motion.div>
+          ) : null}
+          {!isOpen ? (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.5 }}
+            >
+              <Icons iconName="chevronDown" />
+            </motion.div>
+          ) : null}
+        </motion.header>
+        <AnimatePresence initial={false}>
+          {isOpen ? (
+            <motion.section
+              key="content"
+              initial="collapsed"
+              animate="open"
+              exit="collapsed"
+              variants={{
+                open: { opacity: 1, height: "auto" },
+                collapsed: { opacity: 0, height: 0 },
+              }}
+              transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
+              style={{
+                overflow: "hidden",
+              }}
+            >
+              {content}
+            </motion.section>
+          ) : null}
+        </AnimatePresence>
+      </motion.div>
+    </AnimatePresence>
   );
 };
